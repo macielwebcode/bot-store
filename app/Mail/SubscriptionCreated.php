a9a0d11d2ca36694467a0d30674d949dd5bec640
macielwebcode <macielwebcode@gmail.com>
2021-07-01 00:01:42 -0300

    Emails definidos para envio de alertas ao cliente

---
+++ app/Mail/SubscriptionCreated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Subscription;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SubscriptionCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+
+    public $subscription;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Subscription $subscription)
+    {
+        $this->subscription = $subscription;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->view('emails.subscription_created')
+            ->with("subscription", $this->subscription);
+    }
+}
