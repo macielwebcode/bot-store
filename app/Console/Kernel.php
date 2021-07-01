8faa5b035e58882560b1ec67c69d3afbc2520752
macielwebcode <macielwebcode@gmail.com>
2021-06-30 23:56:59 -0300

    Controle admin para criação de planos e protótipo de crons para rodar periodicamente

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,18 @@
 
 namespace App\Console;
 
+use App\Http\Traits\appSettings;
+use App\Mail\LowBalanceAlert;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class Kernel extends ConsoleKernel
 {
+    use appSettings;
     /**
      * The Artisan commands provided by your application.
      *
@@ -24,7 +31,48 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // Run alert mail batch daily to notice users that has balance bellow X percent
+        $schedule->call(function() {
+
+            $warning_percent = $this->app_settings->warning_percent;
+
+            // Gets only users with balance below warning percent
+            $user_bellow_index = User::where("subscriptions.");
+
+            foreach($user_bellow_index as $user){
+
+                Log::info("Sending low balance alert mail to [ ". $user->email . " ]");
+
+                Mail::to($user->email)->send(new LowBalanceAlert($user));
+
+            }
+
+        })->daily();
+
+        // Get user consumption at Yank
+        $schedule->call(function() {
+
+            // TODO: Gets users consumption
+            // Call Yank WS for updates
+            $consumptions = [];
+
+            foreach($consumptions as $con){
+                $user = User::find($con['user_id']);
+                $product = Product::find($con['prod_id']);
+
+                $user->extracts()->create([
+                    'operation' => 'D',
+                    'description' => 'Consumo bot - '. $product->title,
+                    'balance'   => $user->balance - $product->value,
+                    'old_balance' => $user->balance,
+                    'user_id' => $user->id
+                ]);
+
+                $user->balance -= $product->value;
+
+                $user->save();
+            }
+        })->everyMinute();
     }
 
     /**
--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -6,43 +6,78 @@ use App\Http\Controllers\Controller;
 use App\Http\Traits\apiResponser;
 use App\Models\Plan;
 use App\Services\PagarmeRequestService;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class PlanController extends Controller
 {
     use apiResponser;
     public function store(Request $request)
     {
-        DB::beginTransaction();
+        $validator = Validator::make($request->all(), [
+            'value'             => 'required|numeric',
+            'title'             => 'required|string',
+            "description"       => 'required|string',
+            "max_bots"          => 'required|numeric',
+            "max_usage"         => 'required|numeric',
+            "charge_period"     => 'required|numeric',
+            "balance_saving"    => 'required|boolean',
+        ], [
+            'value' => "valor",
+            'title' => "titulo",
+            'description' => "descricao",
+            'max_bots' => "robos ativos",
+            'max_usage' => "maximo de requisicoes",
+            'charge_period' => "duracao do plano",
+            'balance_saving' => "guarda saldo do mes anterior"
+        ]);
 
-        $data = $request->all();
+        if($validator->fails()) {
+            return $this->error("Campo(s) inválidos", 500);
+        } else {
+            DB::beginTransaction();
 
-        $plan = new Plan;
-        $plan->title = $data['title'];
-        $plan->description = $data['description'];
-        $plan->value = $data['value'];
-        $plan->max_bots = $data['max_bots'];
-        $plan->max_usage = $data['max_usage'];
-        $plan->charge_period = $data['charge_period'];
-        $plan->balance_saving = $data['balance_saving'];
+            $data = $request->all();
 
-        $plan->save();
+            $plan = new Plan;
+            $plan->title = $data['title'];
+            $plan->description = $data['description'];
+            $plan->value = $data['value'];
+            $plan->max_bots = 0 + $data['max_bots'];
+            $plan->max_usage = 0 + $data['max_usage'];
+            $plan->charge_period = $data['charge_period'];
+            $plan->balance_saving = $data['balance_saving'];
 
-        $pagarme = new PagarmeRequestService;
-        $plan_request = $pagarme->createPlan($plan->value, $plan->charge_period, $plan->title, 2, 0);
+            $plan->save();
 
-        if(isset($plan_request['errors'])){
-            DB::rollBack();
-            $erro = collect($plan_request['errors'])->pluck("messsage");
-            return $this->error($erro, 500);
+            $pagarme = new PagarmeRequestService;
+
+            try{
+                Log::debug("Criando plano...");
+                $plan_request = $pagarme->createPlan(
+                    $plan->value * 100, 
+                    $plan->charge_period, 
+                    $plan->title, 
+                    2, 
+                    0
+                );
+                Log::info("Plano criado: [ " . json_encode($plan_request) . " ]");
+
+                $plan->external_id = $plan_request->id;
+                $plan->save();
+            } catch(Exception $e) {
+
+                DB::rollBack();
+                // $erro = collect($plan_request->errors)->pluck("messsage");
+                $erro = "Não foi possível gerar plano";
+                return $this->error($erro, 500);
+            }
+
+            DB::commit();
+            return $this->success($plan);
         }
-
-        $plan->external_id = $plan_request['id'];
-        $plan->save();
-
-        DB::commit();
-        return $this->success($plan);
-
     }
 }
